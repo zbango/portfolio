1016291f8bdfa5a441494945b5a06b49c0ca57c1
Steven Tabango <zbango@192.168.1.6>
2024-02-21 23:21:19 -0500

    Initialized react next  project

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="mb-32 grid text-center">
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Page under construction :){" "}
+          </h2>
+          <h6 className={`mb-3`}>
+            Hey there! I'm currently working on this page. Please check back later.
+          </h6>
+          <h6 className={`mb-3`}>
+            Best,
+          </h6>
+          <h6 className={`mb-3`}>
+            Steven (Zbango Systems)
+          </h6>
+      </div>
+    </main>
+  );
+}
