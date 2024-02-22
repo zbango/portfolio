40122fbd96a9d335716c69eb152911dbbe8145d7
Steven Tabango <zbango@192.168.1.6>
2024-02-21 23:25:10 -0500

    Fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Page under construction :){" "}
           </h2>
-          <h6 className={`mb-3`}>
-            Hey there! I'm currently working on this page. Please check back later.
+          <h6 className={`mb-3 font-semibold`}>
+            Hey there! I am currently working on this page. Please check back later.
           </h6>
-          <h6 className={`mb-3`}>
+          <h6 className={`mb-3 font-semibold`}>
             Best,
           </h6>
-          <h6 className={`mb-3`}>
+          <h6 className={`mb-3 font-semibold`}>
             Steven (Zbango Systems)
           </h6>
       </div>
